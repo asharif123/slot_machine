ac9b1d67b6d6ebfea3e5361f7958cfab054e8a73
asharif123 <awadsharif9@gmail.com>
2023-11-30 13:59:38 -0800

    added if conditions to check if user entered horizontal/vertical/diagnol

--- Program.cs
+++ Program.cs
@@ -16,18 +16,14 @@ namespace slot_machine
             //user's winnings, user starts at $50
             int winnings = 50;
 
-            //user decides whether to match numbers horizontally/vertically/diagnolly
-            Console.WriteLine("Enter 'h' to match numbers horizontally, 'v' to match vertically, 'd' to match diagonally\n");
-            char userInput = Char.ToLower(Console.ReadKey().KeyChar);
-
-            //array to store random digits
-            //2D array that is 3 by 3
-            int[,] spinningSlotMachine = new int[3, 3];
-
             //insert values in spinning slot machine array
             //2 second delay
             Console.WriteLine("Spinning the wheel...\n");
             System.Threading.Thread.Sleep(2000);
+
+            //array to store random digits
+            //2D array that is 3 by 3
+            int[,] spinningSlotMachine = new int[3, 3];
             Random rng = new Random();
             for (int i = 0; i <= ROW_COUNT - 1; i++)
             {
@@ -35,8 +31,33 @@ namespace slot_machine
                 {
                     int randomValue = rng.Next(10);
                     spinningSlotMachine[i, j] = randomValue;
+                    Console.WriteLine(spinningSlotMachine[i, j]);
                 }
                 Console.WriteLine();
+            }
+            //user decides whether to match numbers horizontally/vertically/diagnolly
+            Console.WriteLine("Enter 'h' to match numbers horizontally, 'v' to match vertically, 'd' to match diagonally\n");
+            char userInput = Char.ToLower(Console.ReadKey().KeyChar);
+
+            //testcase if user selects horizontal or vertical
+            if (userInput == 'h' || userInput == 'v')
+            {
+                Console.WriteLine("\nEnter 1 to try to match 1 line or 3 to try to match 3 lines!\n");
+                string val = Console.ReadLine();
+                int intToVal = Convert.ToInt32(val);
+            }
+            //testcase if user selects diagnol
+            else if (userInput == 'd')
+            {
+                Console.WriteLine("\nEnter 1 to try to match 1 line or 2 to try to match 2 lines!\n");
+                string val = Console.ReadLine();
+                int intToVal = Convert.ToInt32(val);
+            }
+
+            else
+            {
+                Console.WriteLine("\nPlease enter a valid input!\n");
+                Main();
             }
 
             Console.WriteLine("\nPress y to continue playing or any key to quit!\n");
