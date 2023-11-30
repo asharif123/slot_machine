496bf57b412191ef0cff111fc4cd44c3f882ebbe
asharif123 <awadsharif9@gmail.com>
2023-11-30 11:48:33 -0800

    asking user option to input vertically/horizontally/diagonally

--- Program.cs
+++ Program.cs
@@ -1,29 +1,5 @@
-﻿/*Design a game where the user can play a make believe slot machine. 
- * The user will be asked to make a wager to play various lines in a 3 x 3 grid. 
- * They can play center line, all three horizontal lines, all vertical lines and diagonals.
-For instance the user can enter $3 dollars and play all three horizontal lines. 
-If the top line hits a winning combination, they earn $1 dollar for that line.
+﻿//
 
-rocket Tips: The mention of a grid here should be a dead giveaway that you need a 2D array.
-
-You will also need functionality that can check a horizontal line, a vertical line and a diagonal.
-
-Depending on the number of lines they play, you may need to execute all three of these statements one or multiple times to 
-look for winning lines. If they are playing three lines, you would call your horizontal line check function three times... 
-one for the top row, one for the center row and one for the bottom row. Each of these row checking algorithms will 
-then need to look for winning combinations. The result is then dumped into the player’s money total. 
-As for the mechanism to determine what the wheels produce per spin, use a random number generating function.
-*/
-
-/*
- STEPS:
-- create welcome message to the user
-- create a function for this
-- create random number from 0 to 9 and insert in 2D grid
-- insert random numbers in slot machine
-- create variable to store user's money
-- 
- */
 namespace slot_machine
 {
     internal class Program
@@ -37,8 +13,12 @@ namespace slot_machine
             const int ROW_COUNT = 3;
             const int COLUMN_COUNT = 3;
 
-            //user's winnings, user starts at $10
-            int winnings = 10;
+            //user's winnings, user starts at $50
+            int winnings = 50;
+
+            //user decides whether to match numbers horizontally/vertically/diagnolly
+            Console.WriteLine("Enter 'h' to match numbers horizontally, 'v' to match vertically, 'd' to match diagonally\n");
+            char userInput = Char.ToLower(Console.ReadKey().KeyChar);
 
             //array to store random digits
             //2D array that is 3 by 3
@@ -55,15 +35,14 @@ namespace slot_machine
                 {
                     int randomValue = rng.Next(10);
                     spinningSlotMachine[i, j] = randomValue;
-                    Console.WriteLine(spinningSlotMachine[i, j]);
                 }
                 Console.WriteLine();
             }
 
             Console.WriteLine("\nPress y to continue playing or any key to quit!\n");
-            char userInput = Char.ToLower(Console.ReadKey().KeyChar);
+            char optionToContinue = Char.ToLower(Console.ReadKey().KeyChar);
             //if user wishes to continue, call the main function
-            if (userInput == 'y')
+            if (optionToContinue == 'y')
             {
                 Main();
             }
