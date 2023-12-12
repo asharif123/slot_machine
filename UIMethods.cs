058410effa72c7ce114299b644036355dc70fea5
asharif123 <awadsharif9@gmail.com>
2023-12-11 21:42:50 -0800

    got rid of usings in UIMethods.cs file

--- UIMethods.cs
+++ UIMethods.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace slot_machine
+﻿namespace slot_machine
 {
     public static class UIMethods
     {
